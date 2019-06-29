bbe25bb9b901637a561d663fdc1f335a4403ba8e
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-06-30 00:15:33 +0200

    lil sth not working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Papa from 'papaparse';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+    filtered: [],
+    query: ""
+  }
+
+  setQuery = event => {
+    this.setState({
+      query: event.target.value,
+      filtered: this.state.data.filter(question =>
+        question.toLowerCase().includes(event.target.value.toLowerCase()))
+    })
+  }
+
+  getCsvData = () => {
+    let csvData = require('./data.csv');
+
+    Papa.parse(csvData, {
+      complete: this.getData
+    });
+  }
+
+  componentWillMount() {
+    this.fetchCsv();
+  }
+
+  fetchCsv = () => {
+    return fetch('./data.csv').then(function (response) {
+      let reader = response.body.getReader();
+      let decoder = new TextDecoder('utf-8');
+
+      return reader.read().then(function (result) {
+        return decoder.decode(result.value);
+      });
+    });
+  }
+
+  getData = (result) => {
+    console.log(result)
+    this.setState({ data: result.data });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" value={this.state.query} onChange={this.setQuery} />
+        <ul>
+          {this.state.filtered.map((question, index) => (
+            <li key={index}>{question}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
